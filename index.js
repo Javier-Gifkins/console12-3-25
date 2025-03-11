3bcdd4fd67e33eed32a9b6c8e1ed44405068d89a
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-03-12 10:53:06 +1300

    SDV503 class notes

---
+++ index.js
@@ -0,0 +1,93 @@
+// // const firstname = "Javier"
+// // const lastname = "Gifkins"
+
+// // camelCaseNaming
+// const classCode = "SDV503"
+
+// // invalid variables
+// // first- name
+// // 1_num
+// // num_#_1
+
+// // declaring different variables of different data types
+// let firstname = "Javier"
+// let lastname = "Gifkins"
+// let country = "New Zealand"
+// let age = 26
+// let isStudent = true
+
+// console.log(firstname, lastname, country, age, isStudent)
+
+// const gravity = 9.81
+// const boilingPoint = 100
+// const Pi = 3.14
+// console.log(gravity,boilingPoint,Pi)
+
+// // variables can also be declaring in one line
+
+// let name = "Javier", job = "Bum", live = "Nelson"
+
+// console.log(name, job, live)
+
+
+// declared variable no value
+var cat
+var kebab
+
+/*
+Data Types:
+
+1. primitive data types
+2. non primitive data types
+
+//SECTION - Primitive data types
+means primitive data types are immutable data types
+
+1. numbers - integers floats etc(1,7.1)
+2. strings - strings of characters (any data under ' or ")
+3. booleans - (true, false)
+4. null - empty value or no value
+5. undefeined - declared variable with no value
+
+//SECTION - object references
+means object references are mutable data types
+
+1. objects
+2. functions
+3. arrays 
+let nums = [0,1,2,3]
+    console.log(nums)
+    nums [2] = 10
+*/
+
+//declared variable assigned value  
+
+// let name = "Javier"
+// let lastname = "Gifkins"
+// let food = "Pizza"
+// let drink = "Coffee"
+// let isStudent = true
+// let country = "NZ"
+
+
+// console.log(name)
+// console.log(lastname)
+// console.log(food)
+// console.log(country)
+
+// console.log(country, name, lastname, food, drink)
+
+
+// let myAge = 26
+//     yourAge = 69
+//     catName = "Tom"
+
+// console.log(myAge, yourAge)
+
+// console.log("I am",myAge,"years old"+", and you are",yourAge,"years old")
+
+
+let nums = [0,1,2,3]
+    console.log(nums)
+    nums [2] = 10
+    console.log(nums)
